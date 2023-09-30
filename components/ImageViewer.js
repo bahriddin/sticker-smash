bd04bab4b8516bef3e734e1c811d8f4cc26a86fb
bahriddin <bahriddin.abdiev@ferocia.com.au>
2023-09-30 18:09:02 +1000

    Take a screenshot

---
+++ components/ImageViewer.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Image } from 'react-native';
+
+export default function ImageViewer({ selectedImage, placeholderImageSource }) {
+  const imageSource = selectedImage  ? { uri: selectedImage } : placeholderImageSource;
+
+  return (
+    <Image source={imageSource} style={styles.image} />
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
